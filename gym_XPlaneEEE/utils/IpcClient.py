7ee64154ab6af2c2c29ef460ac5a81a63aa66756
Felix <github@embedded-engineering.de>
2019-06-21 11:18:06 +0200

    replaced some symlinks with the real files

--- gym_XPlaneEEE/utils/IpcClient.py
+++ gym_XPlaneEEE/utils/IpcClient.py
@@ -1,1 +1,146 @@
-/home/felix/git/xPlaneGym/IpcClient.py
+# -*- coding: utf-8 -*-
+import sys
+import socket
+import os
+import os.path
+import json
+import time
+from threading import Thread
+import threading 
+import ctypes 
+from gym_XPlaneEEE.utils.dataCenter import DataCenter
+from gym_XPlaneEEE.utils.singleton import SingletonMixin
+
+
+#TODO remove all references to printFlag
+from gym_XPlaneEEE.utils.xPlaneGymCalculations import prepareInitialPlaneState
+printFlag = 0
+
+class IpcClient(Thread, SingletonMixin):
+    def __init__ (self):
+        Thread.__init__(self)
+        self.continueFlag = True
+        self.dc = DataCenter.instance()
+        self.client = None
+        self.socketName = None
+    
+    def connect(self, socketName):
+        #close an existing connection
+        if self.client:
+            try:
+                self.client.close
+            except Exception as inst:
+                print(type(inst))    # the exception instance
+                print(inst.args)     # arguments stored in .args
+                print(inst)          # __str__ allows args to be printed directly, but may be overridden in exception subclasses
+            self.client = None
+        self.socketName = socketName
+        print ("Connecting...")
+        if os.path.exists(self.socketName):
+            self.client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+            try:
+                self.client.connect(self.socketName)
+            except:
+                print("couldn't connect to ", self.socketName ,". Quitting!")
+                raise ConnectionError("couldn't connect to ", self.socketName ,". Quitting!")
+                # time.sleep(1)   #specify the time between retries here
+                # continue
+        else:
+            raise ConnectionError (self.socketName ," does not exist. Quitting!")
+            # time.sleep(1)
+            # continue
+
+    def getSocketName(self):
+        return self.socketName
+    
+    def run(self):
+        if not self.socketName:
+            raise ConnectionError("No socketName set to connect to. Use setSocketName(sn) before starting listener thread.")
+        global printFlag
+        print ("Ready.")
+        print ("Ctrl-C to quit.")
+        while True:
+            if not self.continueFlag:
+                break
+            datagram = self.client.recv( 2048 )
+            if not datagram:
+                break
+            else:
+                # print ("-" * 20)
+                # print (datagram)
+                try:
+                    socketData = json.loads(datagram[:-1])  #there is an extra `\f` character at the end of the data; see SockServer.cpp
+                    if(socketData['type'] == 'PLANE_STATE'):
+                        if printFlag>0:
+                            print (json.dumps(socketData, sort_keys=True, indent=4))
+                            printFlag -= 1
+                        # print("PLANE_STATE received")
+                        self.dc.putState(socketData['data'])
+                    else:
+                        print (json.dumps(socketData, sort_keys=True, indent=4))
+                except ValueError as inst:
+                    print(type(inst))    # the exception instance
+                    print(inst.args)     # arguments stored in .args
+                    print(inst)          # __str__ allows args to be printed directly, but may be overridden in exception subclasses
+                except Exception as inst:
+                    print(type(inst))    # the exception instance
+                    print(inst.args)     # arguments stored in .args
+                    print(inst)          # __str__ allows args to be printed directly, but may be overridden in exception subclasses
+        print ("Lost Connection!")
+        try:
+            self.client.close
+        except Exception as inst:
+            print(type(inst))    # the exception instance
+            print(inst.args)     # arguments stored in .args
+            print(inst)          # __str__ allows args to be printed directly, but may be overridden in exception subclasses
+        self.client = None
+        print ("Done. Quitting IPC-Client Thread now.")
+    
+    def setContinueFlag(self, flag):
+        self.continueFlag = True if flag == True else False
+        print("continueFlag = %r" % self.continueFlag)
+    
+    def socketSendData(self, msgTypeStr, requestId, dataDict):
+        if self.client == None:
+            print("No connection available. Try again later.")
+            return False
+        else:
+            j = {}
+            j['type']      = msgTypeStr
+            j['requestId'] = requestId
+            j['data']      = dataDict
+            try:
+                # see https://stackoverflow.com/a/42612820/2682209
+                # print(json.dumps(j, indent=4, sort_keys=True))
+                self.client.send(json.dumps(j).encode('utf-8'))
+            except Exception as inst:
+                print(type(inst))    # the exception instance
+                print(inst.args)     # arguments stored in .args
+                print(inst)          # __str__ allows args to be printed directly, but may be overridden in exception subclasses
+                #TODO raise inst
+                return False
+        return True
+
+
+if __name__ == '__main__':
+    #start a listener thread
+    ipcClient = IpcClient.instance()
+    ipcClient.setSocketName("/tmp/eee_AutoViewer")
+    ipcClient.start()
+
+    # youself go watch wheter the program is supposed to stop and gracefully stop the threads
+    keyPress = ''
+    print("Press Ctrl-C to quit")
+    while True:
+        # keyPress = input()
+        # if(keyPress == 'q' or keyPress == 'Q'):
+        #     break
+        time.sleep(15)
+        print("sending message to socket")
+        message = prepareInitialPlaneState()
+        ipcClient.socketSendData(b"SET_PLANE_STATE", 1, message['data'])
+        printFlag = 3
+    ipcClient.setContinueFlag(False)
+
+
+    print ("Stopping Main Thread")
--- gym_XPlaneEEE/utils/dataCenter.py
+++ gym_XPlaneEEE/utils/dataCenter.py
@@ -1,1 +1,110 @@
-/home/felix/git/xPlaneGym/dataCenter.py
+from threading import Lock, Event
+from gym_XPlaneEEE.utils.singleton import SingletonMixin
+import numpy as np
+
+planeStateLock = Lock()
+newDataEvent = Event()
+
+class DataCenter(SingletonMixin):
+    def __init__ (self):
+        self.planeState = None
+        self.observation = 0
+
+    def putState(self, planeState):
+        planeStateLock.acquire()
+        self.planeState = planeState
+        planeStateLock.release()
+        self.observation += 1
+        newDataEvent.set()
+
+    def getState(self):
+        newDataEvent.clear()
+        planeStateLock.acquire()
+        currentState = self.planeState
+        planeStateLock.release()
+        return currentState
+    
+    def getSpeedObservation(self):
+        """
+        Returns
+        -------
+        Returns a single (raw) observation to be used for the speed control. The following datarefs are included:
+          - indicated_airspeed_ms #the speed to be controlled
+          - stallWarning          #The stall warning signal
+          - h_ind                 #Indicated barometric altitude, quite probably in feet actually.
+          - alpha                 #angle of attack
+          - true_theta            #pitch
+          - yoke_pitch_ratio      #The deflection of the joystick axis controlling pitch.
+          - true_phi              #roll
+          - yoke_roll_ratio       #The deflection of the joystick axis controlling roll.
+
+        Normalization of the values needs to be done in a wrapper.
+        """
+        obs = np.zeros(8)
+        planeStateLock.acquire()
+        if self.planeState == None:
+            #it may happen, that there is no observation available yet
+            planeStateLock.release()
+            return obs
+        obs[0] = self.planeState['indicated_airspeed_ms']
+        obs[1] = self.planeState['stallWarning']
+        obs[2] = self.planeState['h_ind']
+        obs[3] = self.planeState['alpha']
+        obs[4] = self.planeState['true_theta']
+        obs[5] = self.planeState['yoke_pitch_ratio']
+        obs[6] = self.planeState['true_phi']
+        obs[7] = self.planeState['yoke_roll_ratio']
+        planeStateLock.release()
+
+        return obs
+
+    def getGlideAngleObservation(self):
+        """
+        Returns
+        -------
+        Returns a single (raw) observation to be used for the speed control. The following datarefs are included:
+        - glide Angle           #the descent angle to be controlled glide Angle = to_degrees(atan(Vh_ind/tas)
+        - stallWarning          #The stall warning signal
+        - tas                   #the true airspeed [m/s]
+        - Vh_ind                #the sinkrate [m/s]
+        - h_ind                 #Indicated barometric altitude, quite probably in feet actually.
+        - alpha                 #angle of attack
+        - true_theta            #pitch
+        - yoke_pitch_ratio      #The deflection of the joystick axis controlling pitch.
+        - true_phi              #roll
+        - yoke_roll_ratio       #The deflection of the joystick axis controlling roll.
+
+        Normalization of the values needs to be done in a wrapper.
+        """
+        obs = np.zeros(10)
+        planeStateLock.acquire()
+        if self.planeState == None:
+            #it may happen, that there is no observation available yet
+            planeStateLock.release()
+            return obs
+        obs[1] = self.planeState['stallWarning']
+        obs[2] = self.planeState['true_airspeed']
+        obs[3] = self.planeState['vh_ind']
+        obs[4] = self.planeState['h_ind']
+        obs[5] = self.planeState['alpha']
+        obs[6] = self.planeState['true_theta']
+        obs[7] = self.planeState['yoke_pitch_ratio']
+        obs[8] = self.planeState['true_phi']
+        obs[9] = self.planeState['yoke_roll_ratio']
+        planeStateLock.release()
+        if obs[2] != 0:
+            #wir müssen den Gleitwinkel selber rechnen aus true_airspeed und sinkrate
+            #der von XPlane ausgegebene Winkel ist bezogen auf ground_speed und damit bei Wind unbrauchbar.
+            obs[0] = np.rad2deg(np.arctan(obs[3]/obs[2]))
+
+        return obs
+
+
+    def awaitNextObservation(self):
+        """Blocks until a new observation is received"""
+        newDataEvent.clear()
+        newDataEvent.wait(1)    #1 second timeout
+        return newDataEvent.isSet()
+
+
+        
